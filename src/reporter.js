0ffd02983d4ad3b0315f241de4ed923d37108b7c
jembijemb <jembi@protonmail.com>
2019-07-19 17:50:37 +0200

    retry to send report indefinitely

--- src/reporter.js
+++ src/reporter.js
@@ -2,30 +2,34 @@
 
 // Internal: Check that cavy-cli server is running and if so, send report.
 
-export default url => async report => {
-  try {
-    const response = await fetch(url);
-    const text = await response.text();
+export default url =>
+  async function doReport(report) {
+    try {
+      const response = await fetch(url);
+      const text = await response.text();
 
-    if (text == "cavy-cli running") {
-      return send(report, url);
-    } else {
-      throw new Error("Unexpected response");
-    }
-  } catch (e) {
-    // If cavy-cli is not running, let people know in a friendly way
-    if (e.message.match(/Network request failed/)) {
-      const message =
-        "Skipping sending test report to cavy-cli - if you'd " +
-        "like information on how to set up cavy-cli, check out the README " +
-        "https://github.com/pixielabs/cavy-cli";
+      if (text == "cavy-cli running") {
+        return send(report, url);
+      } else {
+        throw new Error("Unexpected response");
+      }
+    } catch (e) {
+      // If cavy-cli is not running, let people know in a friendly way
+      if (e.message.match(/Network request failed/)) {
+        const message =
+          "Skipping sending test report to cavy-cli - if you'd " +
+          "like information on how to set up cavy-cli, check out the README " +
+          "https://github.com/pixielabs/cavy-cli";
 
-      console.log(message);
-    } else {
-      console.log(`Skipping sending test report to cavy-cli - ${e.message}.`);
+        console.log(message);
+      } else {
+        console.log(`Skipping sending test report to cavy-cli - ${e.message}.`);
+      }
+      const retryDelayMs = 5000;
+      console.log(`Retrying to send report in ${retryDelayMs / 1000} seconds`);
+      setTimeout(() => doReport(report), retryDelayMs);
     }
-  }
-};
+  };
 
 // Internal: Make a post request to the cavy-cli server with the test report.
 async function send(report, url) {
